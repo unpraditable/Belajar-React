939ef58fc9a68719eec340e162a7cd13eeedc89a
Aditya Pradita S <pradita397@gmail.com>
2018-10-03 14:28:46 +0700

    Belajar Styling
    Belajar styling di react app, baik styling inline maupun styling dengan menggunakan file yang terpisah

--- src/App.js
+++ src/App.js
@@ -36,11 +36,22 @@ class App extends Component {
     }
     
   render() {
+      
+      const style = {
+          backgroundColor: 'white',
+          font: 'inherit',
+          border: '1px solid blue',
+          padding: '8px',
+          cursor: 'pointer',
+      }
+      
     return (
       <div className="App">
         <h1>Hello, I am a React Application</h1>
         <h1>Is this working?</h1>
-        <button onClick={() => this.switchNameHandler('Garrod')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Garrod')}>Switch Name</button>
         <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
