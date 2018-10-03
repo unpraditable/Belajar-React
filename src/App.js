581e87a3a81b41f5c7ab8bd654ad3fae6786d550
Aditya Pradita S <pradita397@gmail.com>
2018-10-03 13:25:22 +0700

    Belajar state dan props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Stephanie', age: 26},
+
+        ],
+        otherState: 'some other value'
+    }
+    
+    switchNameHandler = () => {
+//        console.log('Was clicked!');
+//        this.state.persons[0].name = 'Maximillian'
+        this.setState({
+            persons: [
+                {name: 'Maximilian', age: 28},
+                {name: 'Manu', age: 29},
+                {name: 'Stephanie', age: 27},
+            ],
+        })
+    }
+    
   render() {
     return (
       <div className="App">
         <h1>Hello, I am a React Application</h1>
+        <h1>Is this working?</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
       </div>
     );
   }
