db18c91cfb541447d70d23e81e9f5a8059dce236
Aditya Pradita S <pradita397@gmail.com>
2018-10-03 16:45:38 +0700

    Add method to delete person

--- src/App.js
+++ src/App.js
@@ -14,18 +14,13 @@ class App extends Component {
         showPersons: false
     }
     
-    switchNameHandler = (newName) => {
-//        console.log('Was clicked!');
-//        this.state.persons[0].name = 'Maximillian'
-        this.setState({
-            persons: [
-                {name: newName, age: 28},
-                {name: 'Manu', age: 29},
-                {name: 'Stephanie', age: 27},
-            ],
-        })
+    deletePersonHandler = (personIndex) => {
+//        const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
     }
-    
+   
     nameChangedHandler = (event) => {
         this.setState({
             persons: [
@@ -56,8 +51,9 @@ class App extends Component {
       if (this.state.showPersons) {
           persons = (
             <div>
-              {this.state.persons.map(person => {
+              {this.state.persons.map((person,index) => {
                 return <Person 
+                        click={ () => this.deletePersonHandler(index)}
                         name={person.name} 
                         age={person.age} />
               })}
